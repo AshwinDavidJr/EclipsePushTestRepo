1da6881660112a98fa7addaded18c37f89b185c3
antonychirayil <antonychirayil@users.noreply.github.com>
2021-08-24 22:59:19 +0530

    Test commit made

--- SampleProj/src/Test.java
+++ SampleProj/src/Test.java
@@ -9,6 +9,8 @@ public class Test {
 
  public static void main(String[] args){
 	 System.out.println("Hello Team");
+    String str = "test";
+	System.out.println("String is : "+ str);
      
  }
 }
